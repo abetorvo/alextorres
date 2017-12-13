8e7693ee8ee1d4f79e9af78a11f9c2af53d0e9ad
Alex Torres <abetorvo@hotmail.com>
2017-12-13 12:17:51 +0000

    Add files and make rough draft

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ let express = require('express'),
     app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
     res.render('landing');
 });
 
-app.get('/github', function(req, res){
-    res.redirect('https://github.com/abetorvo/alextorres');
+app.get('/certificates', function(req, res){
+    res.render('certificates');
+});
+
+app.get('/projects/colorgame', function(req, res){
+    res.render('projects/colorgame');
+});
+
+app.get('/projects/tictactoe', function(req, res){
+    res.render('projects/tictactoe');
 });
 
 app.get('*', function(req, res){
